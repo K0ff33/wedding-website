10acb9b1672b3e80e33cb2ea46c7ae8079947ab3
Kamil Wojtczyk <kamil@gearedapp.co.uk>
2018-01-28 23:42:32 +0000

    💄 Fixed copy-pasted CSS properties

--- src/components/HeroLeft/index.js
+++ src/components/HeroLeft/index.js
@@ -3,7 +3,7 @@ import React from 'react'
 import './index.scss'
 
 const HeroLeft = () => (
-  <section className="hero is-fullheight left-fixed">
+  <section className="hero is-success is-fullheight left-fixed">
     <div className="hero-body">
       <div className="container">
         <h1 className="title">Bogusia & Kamil</h1>
