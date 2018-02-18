fb542acdfec420d119b06880b10c9f687b2fe30d
Kamil Wojtczyk <kamil@gearedapp.co.uk>
2018-02-18 23:51:59 +0000

    ✨ Link to Google Calendar event

--- src/components/HeroRight/index.js
+++ src/components/HeroRight/index.js
@@ -17,7 +17,15 @@ const HeroRight = () => (
         Łódź, Poland
       </a>
     </h2>
-    <h3 className="subtitle is-size-5">Saturday, 12th May 2018</h3>
+    <h3 className="subtitle is-size-5">
+      <a
+        href="https://calendar.google.com/calendar/r/eventedit?dates=20180512T153000Z/20180512T160000Z&ctz=Europe%2FWarsaw&location=Registry+Office,+Pabianicka,+%C5%81%C3%B3d%C5%BA,+Poland&text=Bogusia+%26+Kamil's+Wedding&details=We%27re+getting+married!+Please+come+at+least+4.15pm+"
+        target="_blank"
+        style={{ color: 'inherit' }}
+      >
+        Saturday, 12th May 2018
+      </a>
+    </h3>
     <Timer until={new Date(2018, 4, 12, 16, 30)} />
   </section>
 )
