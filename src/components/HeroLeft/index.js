c2c57bbc2d160279f90ef0b0896ad210e55f84d7
Kamil Wojtczyk <kamil@gearedapp.co.uk>
2018-03-12 23:58:38 +0000

    🔥 Removed text on the left side of hero

--- src/components/HeroLeft/index.js
+++ src/components/HeroLeft/index.js
@@ -2,15 +2,6 @@ import React from 'react'
 
 import './index.scss'
 
-const HeroLeft = () => (
-  <section className="hero is-fullheight left-fixed photo">
-    <div className="hero-body">
-      <div className="container">
-        <h1 className="title">Bogusia & Kamil</h1>
-        <h2 className="subtitle">Some welcome text</h2>
-      </div>
-    </div>
-  </section>
-)
+const HeroLeft = () => <section className="hero is-fullheight left-fixed photo" />
 
 export default HeroLeft
