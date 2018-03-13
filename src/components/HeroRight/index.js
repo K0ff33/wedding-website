776997605d7be74d792b6d4ef2564b52b2b93607
Kamil Wojtczyk <kamil@gearedapp.co.uk>
2018-03-13 00:09:01 +0000

    💄 Added SVG to the hero right

--- src/components/HeroRight/index.js
+++ src/components/HeroRight/index.js
@@ -13,13 +13,12 @@ const HeroRight = () => (
     }}
     id="top"
   >
-    <h2 className="subtitle is-size-4">
-      <a href="https://goo.gl/maps/yAxX61ECyeC2" target="_blank" style={{ color: 'inherit' }}>
-        Łódź, Poland
-      </a>
-    </h2>
+    <object type="image/svg+xml" data="/svg/bogusia_kamil.svg">
+      Bogusia &amp; Kamil
+    </object>
     <h3 className="subtitle is-size-5">Saturday, 12 May 2018</h3>
     <Timer until={new Date(2018, 4, 12, 16, 30)} />
+    <h4 className="subtitle is-size-6">❤️</h4>
   </section>
 )
 
