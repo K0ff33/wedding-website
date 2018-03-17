54b49ddc06cdd8df60d6a0139745f92815682480
Kamil Wojtczyk <kamil@gearedapp.co.uk>
2018-03-17 17:21:56 +0000

    ♻️ Ups! Forgot to delete some comments

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -68,29 +68,3 @@ export default class Menu extends React.Component {
     )
   }
 }
-
-{
-  /* <nav className="navbar is-transparent">
-  <div className="navbar-brand">
-    <a className="navbar-item" href="https://bulma.io">
-      <img src="https://bulma.io/images/bulma-logo.png" alt="Bulma: a modern CSS framework based on Flexbox" width="112" height="28">
-    </a>
-    <div className="navbar-burger burger" data-target="navbarExampleTransparentExample">
-      <span></span>
-      <span></span>
-      <span></span>
-    </div>
-  </div>
-
-  <div id="navbarExampleTransparentExample" className="navbar-menu">
-    <div className="navbar-start">
-      <a className="navbar-item" href="https://bulma.io/">
-        Home
-      </a>
-      </div>
-    </div>
-
-    
-  </div>
-</nav> */
-}
