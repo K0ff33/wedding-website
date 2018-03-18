59d6e3d4339124c0d4f620676f8d9f072908a1f9
Kamil Wojtczyk <kamil@gearedapp.co.uk>
2018-03-18 22:18:19 +0000

    🌐 i18n schedule component

--- src/components/Schedule/index.js
+++ src/components/Schedule/index.js
@@ -1,64 +1,28 @@
 import React from 'react'
+import { translate } from 'react-i18next'
+
 import Map from './map'
 
-const events = [
-  {
-    title: 'Wedding Ceremony',
-    time: '4.30pm',
-    text: (
-      <div>
-        <p>We're getting married!</p>
-        <p>Please come at least at 4.15pm</p>
-      </div>
-    ),
-    address: {
-      name: 'Registry Office, Pabianicka 2, Łódź',
-      link: 'https://goo.gl/maps/JH9gs6QSYDN2',
-      lat: 51.737941,
-      lng: 19.4632585
-    },
-    calendarLink:
-      "https://calendar.google.com/calendar/r/eventedit?dates=20180512T153000Z/20180512T160000Z&ctz=Europe%2FWarsaw&location=Registry+Office,+Pabianicka,+%C5%81%C3%B3d%C5%BA,+Poland&text=Bogusia+%26+Kamil's+Wedding+Ceremony&details=We%27re+getting+married!+Please+come+at+least+4.15pm+"
-  },
-  {
-    title: 'Wedding Reception',
-    time: '5.30pm',
-    text: (
-      <div>
-        <p>It's party time!</p>
-        <p>We encourage everyone to grab some drinks and get out on the dance floor.</p>
-        <p>Don't be shy. We're all family and friends.</p>
-      </div>
-    ),
-    address: {
-      name: 'Format, Tymienieckiego 3, Łódź',
-      link: 'https://goo.gl/maps/EbnrVMVeVnK2',
-      lat: 51.7483251,
-      lng: 19.4620527
-    },
-    calendarLink:
-      "https://calendar.google.com/calendar/r/eventedit?dates=20180512T163000Z/20180513T010000Z&ctz=Europe%2FWarsaw&location=Format,+%C5%81%C3%B3d%C5%BA,+Poland&text=Bogusia+%26+Kamil's+Wedding+Reception"
-  }
-]
+import { events } from '../../../static/locales/en/Schedule.json'
 
 const Event = props => (
   <div>
     <div className="columns is-mobile">
       <div className="column">
         <p className="has-text-right" style={{ fontWeight: 600 }}>
-          {props.time}
+          {props.t(`events.${props.i}.time`)}
         </p>
       </div>
       <div className="column is-two-thirds">
-        <p style={{ fontWeight: 600 }}>{props.title}</p>
+        <p style={{ fontWeight: 600 }}>{props.t(`events.${props.i}.title`)}</p>
         <p style={{ margin: '0.5rem 0' }}>
           <a href={props.address.link} target="_blank" style={{ fontSize: '0.95rem' }}>
-            {props.address.name}
+            {props.t(`events.${props.i}.address.name`)}
           </a>
         </p>
-        <p>{props.text}</p>
+        <div dangerouslySetInnerHTML={{ __html: props.t(`events.${props.i}.text`) }} />
         <a href={props.calendarLink} target="_blank" style={{ fontSize: '0.85rem' }}>
-          Add to calendar
+          {props.t('calendar')}
         </a>
       </div>
     </div>
@@ -74,11 +38,11 @@ const Event = props => (
   </div>
 )
 
-const Schedule = () => (
+const Schedule = ({ t }) => (
   <section className="middle-section" id="schedule">
-    <h3 className="title is-size-4 has-text-centered">Schedule</h3>
-    {events.map((el, i) => <Event {...el} key={i} />)}
+    <h3 className="title is-size-4 has-text-centered">{t('title')}</h3>
+    {events.map((el, i) => <Event {...el} key={i} t={t} i={i} />)}
   </section>
 )
 
-export default Schedule
+export default translate('Schedule')(Schedule)
