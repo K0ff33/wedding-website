38d2d448450e6bf0938f2cfaf069f9dc56510c06
Kamil Wojtczyk <kamil@gearedapp.co.uk>
2018-03-18 15:55:05 +0000

    ♻️ Replaced SVG with a smaller PNG

--- src/components/Content/index.js
+++ src/components/Content/index.js
@@ -14,9 +14,7 @@ const Content = () => (
     }}
     id="top"
   >
-    <object type="image/svg+xml" data="/svg/bogusia_kamil.svg">
-      Bogusia &amp; Kamil
-    </object>
+    <img src="/img/Bogusia_and_Kamil.png" alt="Bogusia & Kamil" style={{ margin: '2rem', height: 'auto', width: '300px' }} />
     <h3 className="subtitle is-size-5">Saturday, 12 May 2018</h3>
     <Timer until={new Date(2018, 4, 12, 16, 30)} />
     <h4 className="subtitle is-size-6">❤️</h4>
