5ae9e469e4f43ae8c2c54d1e6790fafaeecc7f07
Kamil Wojtczyk <kamil@gearedapp.co.uk>
2018-03-18 23:25:56 +0000

    🔇 Turned off i18n dev logging

--- src/layouts/i18n.js
+++ src/layouts/i18n.js
@@ -1,8 +1,7 @@
-import i18n from 'i18next';
-import XHR from 'i18next-xhr-backend';
-import LanguageDetector from 'i18next-browser-languagedetector';
-import { reactI18nextModule } from 'react-i18next';
-
+import i18n from 'i18next'
+import XHR from 'i18next-xhr-backend'
+import LanguageDetector from 'i18next-browser-languagedetector'
+import { reactI18nextModule } from 'react-i18next'
 
 i18n
   .use(XHR)
@@ -10,10 +9,10 @@ i18n
   .use(reactI18nextModule) // if not using I18nextProvider
   .init({
     fallbackLng: 'en',
-    debug: true,
+    debug: false,
 
     interpolation: {
-      escapeValue: false, // not needed for react!!
+      escapeValue: false // not needed for react!!
     },
 
     // react i18next special options (optional)
@@ -23,7 +22,6 @@ i18n
       bindStore: 'added removed',
       nsMode: 'default'
     }
-  });
+  })
 
-
-export default i18n;
+export default i18n
