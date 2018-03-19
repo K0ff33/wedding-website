a054f4571becca4e72488955828b2a50ca8bf94f
Kamil Wojtczyk <kamil@gearedapp.co.uk>
2018-03-19 23:35:37 +0000

    💬 Wish list text

--- src/components/WishList/index.js
+++ src/components/WishList/index.js
@@ -4,6 +4,7 @@ import { translate } from 'react-i18next'
 const WishList = ({ t }) => (
   <section className="middle-section" id="wishlist">
     <h3 className="title is-size-4 has-text-centered">{t('title')}</h3>
+    <p className="has-text-centered" dangerouslySetInnerHTML={{ __html: t('text') }} />
   </section>
 )
 
