42e17d3c3f3f83ea32c9d61d23b16f8f98600fc8
Kamil Wojtczyk <kamil@gearedapp.co.uk>
2018-04-23 10:49:47 +0100

    ⚡️ Implemented caching behaviour for translation strings

--- src/layouts/i18n.js
+++ src/layouts/i18n.js
@@ -1,13 +1,37 @@
-import i18n from 'i18next'
-import XHR from 'i18next-xhr-backend'
+import i18next from 'i18next'
+import Backend from 'i18next-chained-backend'
+import LocalStorageBackend from 'i18next-localstorage-backend' // primary use cache
+import XHR from 'i18next-xhr-backend' // fallback xhr load
 import LanguageDetector from 'i18next-browser-languagedetector'
 import { reactI18nextModule } from 'react-i18next'
 
-i18n
-  .use(XHR)
+i18next
+  .use(Backend)
   .use(LanguageDetector)
   .use(reactI18nextModule) // if not using I18nextProvider
   .init({
+    backend: {
+      backends: [
+        LocalStorageBackend, // primary
+        XHR
+      ],
+      backendOptions: [
+        {
+          // prefix for stored languages
+          prefix: 'i18next_res_',
+          // expiration
+          expirationTime: 7 * 24 * 60 * 60 * 1000,
+          // language versions
+          versions: {
+            en: 'v1.0',
+            pl: 'v1.0'
+          }
+        },
+        {
+          loadPath: '/locales/{{lng}}/{{ns}}.json' // xhr load path for my own fallback
+        }
+      ]
+    },
     fallbackLng: 'en',
     debug: false,
 
@@ -24,4 +48,4 @@ i18n
     }
   })
 
-export default i18n
+export default i18next
